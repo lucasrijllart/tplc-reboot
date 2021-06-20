2972aeb8d3c89512106fbb2ee9d1cbff59458388
Lucas Rijllart <contact@lucasrijllart.com>
2021-06-20 21:25:32 +0100

    update script

--- tplc-reboot-webdriver.py
+++ tplc-reboot-webdriver.py
@@ -8,48 +8,54 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-WAIT = 15
 
-# start webdriver
-driver_path = "/usr/bin/geckodriver"
-print("Starting webdriver with executable:", driver_path)
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(executable_path=driver_path, options=options)
-driver.set_window_size(1000, 1000)  # smaller can hide login button
+def main():
+    # constants
+    wait = 15  # seconds
+    driver_path = "/usr/bin/geckodriver"
+    tplc_default_ip = "192.168.0.93"
+    progress_wait = 10  # seconds
 
-# get TPLC page
-tplc_ip = sys.argv[1] if len(sys.argv) >= 2 else "192.168.0.93"
-print("Getting login page using IP:", tplc_ip)
-driver.get(f"http://{tplc_ip}/")
+    # start webdriver
+    print("Starting webdriver with executable:", driver_path)
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(executable_path=driver_path, options=options)
+    driver.set_window_size(1000, 1000)  # smaller can hide login button
 
-# login
-print("Writing password")
-try:
-    driver.find_element_by_id("pcPassword").send_keys("admin")
-except NoSuchElementException:
-    print("Could not find pcPassword element, this is generally because another browser is logged in")
-    exit()
-button_login = WebDriverWait(driver, WAIT).until(EC.element_to_be_clickable((By.ID, "loginBtn")))
-button_login.click()
-print("Clicked log in")
+    # get TPLC page
+    tplc_ip = sys.argv[1] if len(sys.argv) >= 2 else tplc_default_ip
+    print("Getting login page using IP:", tplc_ip)
+    driver.get(f"http://{tplc_ip}/")
 
-# reboot
-WebDriverWait(driver, WAIT).until(EC.invisibility_of_element((By.CLASS_NAME, "loading-container-inner")))
-print("Main page loaded, clicking reboot")
-button_reboot = WebDriverWait(driver, WAIT).until(EC.element_to_be_clickable((By.ID, "top-control-reboot")))
-button_reboot.click()
-print("Clicked reboot, clicking yes")
-button_ok = WebDriverWait(driver, WAIT).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-msg-ok")))
-button_ok.click()
-print("Reboot started")
+    # login
+    print("Writing password")
+    try:
+        driver.find_element_by_id("pcPassword").send_keys("admin")
+    except NoSuchElementException:
+        print("Could not find pcPassword element, this is generally because another browser is logged in")
+        exit()
+    button_login = WebDriverWait(driver, wait).until(EC.element_to_be_clickable((By.ID, "loginBtn")))
+    button_login.click()
+    print("Clicked log in")
 
-# print progress from popup
-progress_wait = 10  # seconds
-print(f"Waiting {progress_wait} seconds")
-time.sleep(progress_wait)
-print("Progress:", driver.find_element_by_class_name("progressbar-percentage").text)
+    # reboot
+    WebDriverWait(driver, wait).until(EC.invisibility_of_element((By.CLASS_NAME, "loading-container-inner")))
+    print("Main page loaded, clicking reboot")
+    button_reboot = WebDriverWait(driver, wait).until(EC.element_to_be_clickable((By.ID, "top-control-reboot")))
+    button_reboot.click()
+    print("Clicked reboot, clicking yes")
+    button_ok = WebDriverWait(driver, wait).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-msg-ok")))
+    button_ok.click()
+    print("Reboot started")
 
-# reboot in progress, exit
-print("Quitting")
-driver.quit()
+    # print progress from popup
+    print(f"Waiting {progress_wait} seconds")
+    time.sleep(progress_wait)
+    print("Progress:", driver.find_element_by_class_name("progressbar-percentage").text)
+
+    # reboot in progress, exit
+    print("Quitting")
+    driver.quit()
+    
+main()
